532c8d6808402d37fab0f30bcc4c0f3bee0f3b21
James Johnson <jtaylorjohnson@gmail.com>
2020-04-06 00:46:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,13 +3,11 @@ class Anagram
   
   def initialize(word)
     @word = word 
-    
   end
   
   def match(array)
-  
-  
+    array.select do |word|
+      word.split(" ").sort == @word.split(" ").sort
+    end
   end  
-  
-  
 end
